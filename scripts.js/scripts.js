b9f6d6c8936ddb236b163b67083cd2771545fe06
nandithamadhu123 <iamnanditha2023@gmail.com>
2024-10-23 06:01:10 -1100

    Add portfolio files

---
+++ scripts.js/scripts.js
@@ -0,0 +1,8 @@
+// Handle the form submission (this part can be expanded later)
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Message sent! Thank you for contacting me.');
+});
+
+
+      
